f0bef488922b94d0bd5940f0fe07b2f187f53597
Isaac Reis <isaacreis209@gmail.com>
2026-05-15 00:28:59 -0300

    Sons de Click no Mouse e no Menu

---
+++ Seleção/Assets/Scripts/CickSound.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using FMODUnity;
+
+public class ClickSound : MonoBehaviour {
+    [SerializeField] private EventReference soundEvent;
+
+    void Update() {
+        if (Mouse.current.leftButton.wasPressedThisFrame) {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+
+            if (hit.collider != null && hit.collider.gameObject == gameObject) {
+                RuntimeManager.PlayOneShot(soundEvent, transform.position);
+            }
+        }
+    }
+}
