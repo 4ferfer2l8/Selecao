0999468e63008f35bfaaf6dab9d5838e2074da1f
Isaac Reis <isaacreis209@gmail.com>
2026-05-26 23:25:43 -0300

    Sistema de Aprovar ou não no papel

--- Seleção/Assets/Scripts/NPCControle.cs
+++ Seleção/Assets/Scripts/NPCControle.cs
@@ -3,24 +3,24 @@ using UnityEngine.InputSystem;
 
 public class NPCControle : MonoBehaviour {
     public float velocidade = 2f;
-
     public Transform pontoParada;
     public Transform pontoSaida;
-
     private bool liberado = false;
     private bool chegouNaParada = false;
 
     void Update() {
         if (!chegouNaParada) {
             MoverPara(pontoParada.position);
-
             if (Vector2.Distance(transform.position, pontoParada.position) < 0.1f) {
                 chegouNaParada = true;
             }
         } else if (!liberado) {
-            // 👇 INPUT NOVO
             if (Keyboard.current.eKey.wasPressedThisFrame) {
-                Liberar();
+                if (StampManager.instance.jaCarimbou) {
+                    Liberar();
+                } else {
+                    Debug.Log("Carimbe o documento primeiro!");
+                }
             }
         } else {
             MoverPara(pontoSaida.position);
--- Seleção/Assets/Scripts/SomDePapel.cs
+++ Seleção/Assets/Scripts/SomDePapel.cs
@@ -1,59 +1,39 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 using FMODUnity;
 
-public class SomDePapel : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler
-{
+public class SomDePapel : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler {
     [Header("Eventos FMOD")]
-    [SerializeField] private string eventoHover  = "event:/Seleção_Audios/SFX/Papel_Cartão_Mouse/Papel_Pegar_1";
-    [SerializeField] private string eventoAbrir  = "event:/Seleção_Audios/SFX/Papel_Cartão_Mouse/Papel_Pegar_2";
+    [SerializeField] private string eventoHover = "event:/Seleção_Audios/SFX/Papel_Cartão_Mouse/Papel_Pegar_1";
+    [SerializeField] private string eventoAbrir = "event:/Seleção_Audios/SFX/Papel_Cartão_Mouse/Papel_Pegar_2";
     [SerializeField] private string eventoFechar = "event:/Seleção_Audios/SFX/Papel_Cartão_Mouse/Cartão_Pegar_1";
-
-
 
     [Header("Documento Expandido")]
     [SerializeField] private GameObject painelDocumento;
 
     private bool estaAberto = false;
 
-    public void OnPointerEnter(PointerEventData eventData)
-    {
+    public void OnPointerEnter(PointerEventData eventData) {
         RuntimeManager.PlayOneShot(eventoHover);
     }
 
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        if (!estaAberto)
-        {
+    public void OnPointerClick(PointerEventData eventData) {
+        if (!estaAberto) {
             estaAberto = true;
             RuntimeManager.PlayOneShot(eventoAbrir);
             AbrirDocumento();
         }
-        else
-        {
-            estaAberto = false;
-            RuntimeManager.PlayOneShot(eventoFechar);
-            FecharDocumento();
-        }
+        // fechamento só pelo botão de fechar, não pelo clique no papel
     }
 
-    public void FecharDocumentoExterno()
-    {
-    estaAberto = false;
-    Debug.Log("Documento fechado");
-    }
-
-    private void AbrirDocumento()
-    {
-        painelDocumento.SetActive(true);
-        Debug.Log("Documento aberto");
-    }
-
-    private void FecharDocumento()
-    {
+    public void FecharDocumentoExterno() {
+        estaAberto = false;
         painelDocumento.SetActive(false);
         Debug.Log("Documento fechado");
     }
-    
+
+    private void AbrirDocumento() {
+        painelDocumento.SetActive(true);
+        Debug.Log("Documento aberto");
+    }
 }
