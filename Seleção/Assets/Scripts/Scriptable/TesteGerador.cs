312f6c2f19fc58e477e290f3b07aabfae084c1d3
Isaac Reis <isaacreis209@gmail.com>
2026-06-04 11:42:53 -0300

    bug de spamamento de npc corrigindokkkk

--- Seleção/Assets/Scripts/Scriptable/TesteGerador.cs
+++ Seleção/Assets/Scripts/Scriptable/TesteGerador.cs
@@ -4,9 +4,10 @@ using UnityEngine.InputSystem;
 public class TesteGerador : MonoBehaviour {
     public GameObject npcPrefab;
     public Transform pontoSpawn;
-    public SomDePapel somDePapel; // arrasta o PapelMesaPH_0 aqui no Inspector
+    public SomDePapel somDePapel;
 
     private GeradorDeIndividuos geradorDeIndividuos;
+    private GameObject npcAtual; // NOVO
 
     void Start() {
         geradorDeIndividuos = GetComponent<GeradorDeIndividuos>();
@@ -15,21 +16,30 @@ public class TesteGerador : MonoBehaviour {
 
     void Update() {
         if (Keyboard.current.spaceKey.wasPressedThisFrame)
-            GerarNovoNPC();
+        {
+            if (npcAtual == null)
+            { // só spawna se não tiver nenhum
+                GerarNovoNPC();
+            }
+            else
+            {
+                Debug.Log("Aguarde o NPC atual sair!");
+            }
+        }
     }
 
     void GerarNovoNPC() {
-        GameObject npc = Instantiate(npcPrefab, pontoSpawn.position, Quaternion.identity);
+        npcAtual = Instantiate(npcPrefab, pontoSpawn.position, Quaternion.identity);
 
-        NPCControle controle = npc.GetComponent<NPCControle>();
+        NPCControle controle = npcAtual.GetComponent<NPCControle>();
         controle.pontoParada = GameObject.Find("PontoParada").transform;
         controle.pontoSaida = GameObject.Find("PontoSaida").transform;
 
-        GeradorDeAparencia aparencia = npc.GetComponent<GeradorDeAparencia>();
+        GeradorDeAparencia aparencia = npcAtual.GetComponent<GeradorDeAparencia>();
         aparencia.GerarAparenciaAleatoria();
 
         Individuo individuo = geradorDeIndividuos.GerarIndividuo();
-        somDePapel.DefinirIndividuo(individuo); // passa o documento sorteado pro papel
+        somDePapel.DefinirIndividuo(individuo);
 
         Debug.Log($"NPC criado: {individuo.codigo}");
     }
